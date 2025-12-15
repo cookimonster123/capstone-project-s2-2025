b715ef87976f8f64d52afca71f216268cb0c94e5
cookimonster123 <guanshanhai2003@gmail.com>
2025-12-15 22:57:40 +1300

    Going back to mui ggs

--- client/src/pages/ProjectGalleryPage.tsx
+++ client/src/pages/ProjectGalleryPage.tsx
@@ -2,14 +2,18 @@ import { ProjectGrid } from "@components";
 import { getAllProjects } from "@/api";
 import type { Project } from "@types";
 import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks";
+import TextField from "@mui/material/TextField";
 
 export function ProjectGalleryPage() {
    const [projects, setProjects] = useState<Project[]>([]);
+   const searchedProjects = useDebounce(projects, 500);
 
    const onProjectClick = (project: Project) => {
       console.log("Project clicked:", project.name);
    };
 
+   // First render
    useEffect(() => {
       const fetchProjects = async () => {
          try {
